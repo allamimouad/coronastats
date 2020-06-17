79099f335a00f54042ffb3cbe6e1250d37db92e3
allamimouad <allamimouad@gmail.com>
2020-06-17 14:08:43 +0100

    create region table and all type that we will need

---
+++ DataBase/types_and_tables.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE TYPE corona_case AS OBJECT(
+    c_date DATE,
+    c_confirmed NUMBER,
+    c_deaths NUMBER,
+    c_recovered NUMBER
+);
+
+CREATE OR REPLACE TYPE nt_corona_case AS TABLE OF corona_case; 
+
+CREATE OR REPLACE TYPE city AS OBJECT (
+  c_name   VARCHAR2(40),
+  c_corona_cases      nt_corona_case
+);
+
+CREATE OR REPLACE TYPE nt_city AS TABLE OF city;
+
+CREATE TABLE region (
+  c_name   VARCHAR2(25),
+  c_cities     nt_city)
+  NESTED TABLE c_cities STORE AS cities_tab
+   (NESTED TABLE c_corona_cases STORE AS corona_cases_tab);
+   
+ /*     
+TYPE record_corona_case IS RECORD(
+    c_date DATE,
+    c_confirmed NUMBER,
+    c_deaths NUMBER,
+    c_recovered NUMBER
+); 
+
+*/
